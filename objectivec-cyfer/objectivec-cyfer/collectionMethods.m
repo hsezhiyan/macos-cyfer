4b8833a458b4df75428d179cb3f164e6c2ccc313
hsezhiyan <40924035+hsezhiyan@users.noreply.github.com>
2018-07-17 23:46:08 -0700

    Elapsed Time Functionality
    This class is now able to measure elapsed time spent on different applications.

--- objectivec-cyfer/objectivec-cyfer/collectionMethods.m
+++ objectivec-cyfer/objectivec-cyfer/collectionMethods.m
@@ -9,10 +9,33 @@
 #import <Foundation/Foundation.h>
 #include <Cocoa/Cocoa.h>
 #import "collectionMethods.h"
+#import "timerMethods.h"
+#import "dataStruct.h"
+
+timerMethods *timerMethod = nil;
+dataStruct *dataDict = nil;
+CFTimeInterval startTime;
+CFTimeInterval endTime;
+CFTimeInterval previousElapsedTime;
 
 @implementation collectionMethods
 
-- (id)init {
++ (void) initialize {
+    if(!timerMethod) {
+        timerMethod = [[timerMethods alloc] init];
+    }
+    if(!dataDict) {
+        dataDict = [[dataStruct alloc] init];
+    }
+    if (!startTime) {
+        startTime = [timerMethod getCurrentTime];
+    }
+    if (!endTime) {
+        endTime = [timerMethod getCurrentTime];
+    }
+}
+
+- (id) init {
     if ((self = [super init])) {
         [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
                                                                selector:@selector(appDidActivate:)
@@ -23,31 +46,34 @@
     return self;
 }
 
-- (void)dealloc {
+- (void) dealloc {
     [[[NSWorkspace sharedWorkspace] notificationCenter] removeObserver:self];
     //[super dealloc];
 }
 
-- (void)appDidActivate:(NSNotification *)notification {
+- (void) appDidActivate:(NSNotification *)notification {
+    endTime = [timerMethod getCurrentTime];
+    CFTimeInterval elapsedTime = [timerMethod getElapsedTime: startTime andTime2:endTime];
     NSDictionary *userInfo = [notification userInfo];
-    
+    NSString* processedInfo = [dataDict preprocessing: userInfo];
+    NSLog(@"Processed application name: %@", processedInfo);
+    NSLog(@"Amount of time spent on app: %f", elapsedTime);
     NSLog(@"userInfo == %@", userInfo);
+    startTime = endTime;
 }
-
-
 
 /*
  (Supposed to) return the frontmost application. Currently does not get updated after first application.
-*/
--(NSRunningApplication*) frontmostApplication {
+ */
+- (NSRunningApplication*) frontmostApplication {
     return [NSWorkspace sharedWorkspace].frontmostApplication;
 }
 
 /*
  The following function returns a mutable array (an array without a fixed size) containing paths to all running applications.
  TODO: Processing of this data (we only want applications from the applications folder)
-*/
--(NSMutableArray*) allRunningApplications {
+ */
+- (NSMutableArray*) allRunningApplications {
     NSMutableArray *applications = [NSMutableArray array];
     for (NSRunningApplication *app in
          [[NSWorkspace sharedWorkspace] runningApplications]) {
