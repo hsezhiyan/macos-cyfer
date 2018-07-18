540b2e01b260d47edbf98f4fa79984deda33cd93
hsezhiyan <40924035+hsezhiyan@users.noreply.github.com>
2018-07-17 23:48:06 -0700

    Slimed down main.m
    No longer contains unnecessary functions or method calls.

--- objectivec-cyfer/objectivec-cyfer/main.m
+++ objectivec-cyfer/objectivec-cyfer/main.m
@@ -7,27 +7,11 @@
 static collectionMethods *collectionMethod;
 int main() {
     collectionMethod = [[collectionMethods alloc] init];
-    //timerMethods *timerMethod = [[timerMethods alloc] init];
-    NSRunningApplication *output = [[NSRunningApplication alloc] init];
-    //NSMutableArray *output = [[NSMutableArray alloc] init];
-    //CFTimeInterval startTime;
-    //CFTimeInterval endTime;
-    //CFTimeInterval elapsedTime;
     @autoreleasepool
     {
         NSRunLoop *runLoop = NSRunLoop.currentRunLoop;
-        collectionMethods *cm = [[collectionMethods alloc] init];
-        while ([runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:2]]);
+        //collectionMethods *cm = [[collectionMethods alloc] init];
+        while ([runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]]);
     };
-//    for (int i = 1; i <= 10; i--) {
-//        usleep(2000000);
-//        output = [collectionMethod frontmostApplication];
-//        //output = [collectionMethod allRunningApplications];
-//        //startTime = [timerMethod getCurrentTime];
-//        //endTime = [timerMethod getCurrentTime];
-//        //elapsedTime = [timerMethod getElapsedTime: startTime andTime2:endTime];
-//        //NSLog(@"Frontmost app or list of running apps: %@\n", output);
-//        //NSLog(@"Elapsed time: %f seconds.", elapsedTime);
-//    }
     return 0;
 }
