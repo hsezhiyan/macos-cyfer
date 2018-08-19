9c5c48313c12c274537c42d43ad4135d099d7ea5
hsezhiyan <40924035+hsezhiyan@users.noreply.github.com>
2018-08-18 23:36:05 -0700

    Create serverComm.h

---
+++ objectivec-cyfer/objectivec-cyfer/serverComm.h
@@ -0,0 +1,13 @@
+//
+//  serverComm.h
+//  objectivec-cyfer
+//
+//  Created by Hariharan Sezhiyan on 8/14/18.
+//  Copyright © 2018 Cyfer. All rights reserved.
+//
+
+@interface serverComm : NSObject
+
+- (void) postDict: (NSDictionary*) timerDict;
+
+@end
