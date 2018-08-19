10741ff4b2fb3d1971fd3b3e2ccf5e48f9636ab7
hsezhiyan <40924035+hsezhiyan@users.noreply.github.com>
2018-08-18 23:37:18 -0700

    Update main.m

--- objectivec-cyfer/objectivec-cyfer/main.m
+++ objectivec-cyfer/objectivec-cyfer/main.m
@@ -1,17 +1,11 @@
 #include <Cocoa/Cocoa.h>
 #import "collectionMethods.h"
 #import "timerMethods.h"
+#import "runLoop.h"
 
-// Add observer to register when the frontmostApplication changes
-// [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self selector:@selector(nsworkspaceNotification:) name:nil object:nil];
-static collectionMethods *collectionMethod;
+static runLoop *cyferProgram;
 int main() {
-    collectionMethod = [[collectionMethods alloc] init];
-    @autoreleasepool
-    {
-        NSRunLoop *runLoop = NSRunLoop.currentRunLoop;
-        //collectionMethods *cm = [[collectionMethods alloc] init];
-        while ([runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]]);
-    };
-    return 0;
+    cyferProgram = [[runLoop alloc] init];
+    [cyferProgram runProgram];
 }
+
