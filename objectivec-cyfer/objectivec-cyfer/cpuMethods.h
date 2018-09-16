9dff088d1bef9ae3021d80ee8922a7bfa0ac3b7b
Zhongyu Chen <zhc180@ucsd.edu>
2018-09-16 18:01:54 +0800

    cpuMethods file added

---
+++ objectivec-cyfer/objectivec-cyfer/cpuMethods.h
@@ -0,0 +1,12 @@
+//
+//  cpuMethods.h
+//  objectivec-cyfer
+//
+//  Created by Zhongyu Chen on 2018/9/16.
+//  Copyright © 2018 Cyfer. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface cpuMethods : NSObject
+
+@end
---
+++ objectivec-cyfer/objectivec-cyfer/cpuMethods.m
@@ -0,0 +1,20 @@
+//
+//  cpuMethods.m
+//  objectivec-cyfer
+//
+//  Created by Zhongyu Chen on 2018/9/16.
+//  Copyright © 2018 Cyfer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cpuMethods.h"
+
+#include <Cocoa/Cocoa.h>
+
+@implementation timerMethods
+
+
+
+@end
+
+
